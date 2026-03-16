2cb6aed1a9009558fa45d33ab9522d73b28aba7e
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 00:43:57 +0530

    creating a page to hold past holdings

---
+++ Desktop/POrtfolio Manager/frontend/src/pages/PastHoldingPage.jsx
@@ -0,0 +1,25 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+const PastHolding = () => {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNotes = async() => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  return (
+    <div className="min-h-screen ">
+      <div className="max-w-7xl mx-auto p-4 mb-6 bg-black">
+        {}
+      </div>   
+    </div>
+  )
+}
+
+export default PastHolding
