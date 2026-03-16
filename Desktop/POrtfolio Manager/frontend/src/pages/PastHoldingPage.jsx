b4bbc7a9c822f90aed9ad25b27cb1643741ac156
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 00:47:13 +0530

    function to fetch holdings

--- Desktop/POrtfolio Manager/frontend/src/pages/PastHoldingPage.jsx
+++ Desktop/POrtfolio Manager/frontend/src/pages/PastHoldingPage.jsx
@@ -7,12 +7,21 @@ const PastHolding = () => {
 
   const fetchNotes = async() => {
     try {
-      
+      const res = await axios.get("http://localhost:5001/api/portfolio");
+      setNotes(res.data);
+      console.log(res.data);
     } catch (error) {
-      
+      console.error(error);
+      alert("Failed to fetch past holdings", error);
+    } finally {
+      setLoading(false);
     }
   }
 
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  
   return (
     <div className="min-h-screen ">
       <div className="max-w-7xl mx-auto p-4 mb-6 bg-black">
