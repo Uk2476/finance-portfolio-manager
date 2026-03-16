22a2bbfdaf45e7a6a3347356600c5a154b981e10
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 04:19:24 +0530

    styling of sold block

--- Desktop/POrtfolio Manager/frontend/src/pages/SellingInfoPage.jsx
+++ Desktop/POrtfolio Manager/frontend/src/pages/SellingInfoPage.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router";
+import { Link } from "react-router";
 
 const SellingInfoPage = () => {
     const [name, setName] = useState("");
@@ -31,8 +32,30 @@ const SellingInfoPage = () => {
     } 
 
   return (
-    <div>
-      
+    <div  className="min-h-screen flex items-center justify-center">
+        <div className="container mx-auto px-4 py-8">
+            <div className="max-w-xl mx-auto bg-orange-100 rounded-lg shadow-md p-6">
+                <Link to="/" className="btn btn-ghost bg-stone-200 mb-4 ml-1 mt-1">Back to Home</Link>
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="card-title mb-4 text-2xl">Sold</h2>
+                        <form onSubmit={update}>
+                            <div className="form-control mb-3 shadow-sm">
+                                <label htmlFor="SellingAmount">Selling Amount</label>
+                                <input
+                                    type = "number"
+                                    placeholder="Enter amount"
+                                    value = {SellingAmount}
+                                    onChange={(e) => setSellingAmount(e.target.value)}
+                                    className="input input-bordered"
+                                />
+                            </div> 
+                            <button type="submit" className="btn btn-primary mt-4">Update</button>
+                        </form>    
+                   </div>
+                </div>                
+            </div>
+        </div>          
     </div>
   )
 }
