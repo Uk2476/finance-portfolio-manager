ef78695895e84f80b14a4e83502f00eca69b8f78
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 04:44:59 +0530

    redifined function

--- Desktop/POrtfolio Manager/frontend/src/pages/PastHoldingPage.jsx
+++ Desktop/POrtfolio Manager/frontend/src/pages/PastHoldingPage.jsx
@@ -1,15 +1,16 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
 import SoldCard from "../components/SoldCard";
+import { Link } from "react-router";
 
 const PastHolding = () => {
-  const [notes, setNotes] = useState([]);
+  const [holdings, setHoldings] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchNotes = async() => {
+  const fetchHoldings = async() => {
     try {
       const res = await axios.get("http://localhost:5001/api/portfolio");
-      setNotes(res.data);
+      setHoldings(res.data);
       console.log(res.data);
     } catch (error) {
       console.error(error);
@@ -20,15 +21,19 @@ const PastHolding = () => {
   }
 
   useEffect(() => {
-    fetchNotes();
+    fetchHoldings();
   }, []);
 
   return (
     <div className="min-h-screen ">
+      <h1 className="text-4xl font-bold mb-4 mt-4 ml-4">Past Holdings</h1>
+      <Link to="/" className="btn btn-ghost bg-stone-200 mb-4 ml-4 mt-1">
+        Back to Home
+      </Link>
       <div className="max-w-7xl mx-auto p-4 mb-6 ">
-        {notes.length>0 && (
+        {holdings.length>0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map((note) => (
+            {holdings.map((note) => (
               note.SellingAmount != 0 &&
                 <SoldCard key={note._id} holding={note} />
             ))}
