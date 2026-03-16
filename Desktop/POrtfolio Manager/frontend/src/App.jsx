cbb1a715cf15f417d10b3a609f1d6950b0489471
Uk2476 <kuttkarsh028@gmail.com>
2026-03-16 17:09:14 +0530

    added 5 different routes

--- Desktop/POrtfolio Manager/frontend/src/App.jsx
+++ Desktop/POrtfolio Manager/frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
-import { Divide } from "lucide-react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+
 
 const App =  () => {
     return (<div>
         <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element= {<CreatePage />} />
+            <Route path="/updateinfo" element= {<UpdateInfoPage />} />
+            <Route path="/sellinginfo" element= {<SellingInfoPage />} />
+            <Route path="/delete" element= {<DeletePage />} />
+        </Routes>
     </div>)
 }
 
