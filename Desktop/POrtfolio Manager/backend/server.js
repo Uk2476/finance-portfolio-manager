e936a5f1167562788143f4ea621fda6c962ae510
Uk2476 <kuttkarsh028@gmail.com>
2026-03-16 12:28:55 +0530

    corrected port location

--- Desktop/POrtfolio Manager/backend/server.js
+++ Desktop/POrtfolio Manager/backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { connectDb } from './config/db.js';
+import notesRoutes from './routes/notesRoutes.js'
 
 const app = express();
 
 app.use(express.json());
-app.use('/')
+app.use('/api/portfolio', notesRoutes);
 
 connectDb().then(() => {
     app.listen(5001 ,() => {
