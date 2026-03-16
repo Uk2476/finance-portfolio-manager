b9ef37b2820d1a629877f75844bd0976afc87301
Uk2476 <kuttkarsh028@gmail.com>
2026-03-16 17:01:26 +0530

    basic app.jsx

---
+++ Desktop/POrtfolio Manager/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import { Divide } from "lucide-react";
+
+const App =  () => {
+    return (<div>
+        <Navbar />
+    </div>)
+}
+
+export default App;
