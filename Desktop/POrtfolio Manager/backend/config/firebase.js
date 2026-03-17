f1ec364cffaa5b39e5d579b8d4a113b8cd24eafb
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 19:46:05 +0530

    deplotying in render

--- Desktop/POrtfolio Manager/backend/config/firebase.js
+++ Desktop/POrtfolio Manager/backend/config/firebase.js
@@ -1,13 +1,11 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync("serviceAccountKey.json", "utf-8")
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 const db = admin.firestore();
 export default db;
--- Desktop/POrtfolio Manager/backend/server.js
+++ Desktop/POrtfolio Manager/backend/server.js
@@ -4,13 +4,14 @@ import notesRoutes from './routes/notesRoutes.js';
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: process.env.CLIENT_URL || "http://localhost:5173"
 }));
 app.use('/api/portfolio', notesRoutes);
 
-app.listen(5001 ,() => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
