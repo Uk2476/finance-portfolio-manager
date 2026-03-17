8779eb6c3522a0fc8ffeaa5f4f79025f3d557473
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 21:10:49 +0530

    added cors origin

--- Desktop/POrtfolio Manager/backend/server.js
+++ Desktop/POrtfolio Manager/backend/server.js
@@ -8,8 +8,15 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173"
+    origin: [
+        "http://localhost:5174",
+        "http://localhost:5173",                            // Local development
+        "https://finance-portfolio-manager.onrender.com",        // Your future Render URL
+        "https://finance-portfolio-manager.vercel.app"      // Or your Vercel URL
+    ],
+    credentials: true
 }));
+
 app.use('/api/portfolio', notesRoutes);
 
 app.listen(PORT, '0.0.0.0', () => {
