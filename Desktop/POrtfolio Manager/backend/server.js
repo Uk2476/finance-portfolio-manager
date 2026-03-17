fa61d1cc1f0ecfd4155c20af7ada172bce89f712
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 20:12:26 +0530

    fixed port address

--- Desktop/POrtfolio Manager/backend/server.js
+++ Desktop/POrtfolio Manager/backend/server.js
@@ -12,6 +12,6 @@ app.use(cors({
 }));
 app.use('/api/portfolio', notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
