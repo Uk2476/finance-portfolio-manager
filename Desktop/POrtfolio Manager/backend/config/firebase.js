a1226d358b78398e0ae3c752762309a76879faa3
Uk2476 <kuttkarsh028@gmail.com>
2026-03-17 20:23:11 +0530

    error in serviceACCOUNT

--- Desktop/POrtfolio Manager/backend/config/firebase.js
+++ Desktop/POrtfolio Manager/backend/config/firebase.js
@@ -2,10 +2,11 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 const db = admin.firestore();
 export default db;
